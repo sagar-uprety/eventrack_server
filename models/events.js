0702f83734da8dcca4bb4e096ee86e03fef75b33
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2021-05-29 14:03:01 +0545

    Actual Models added

--- models/events.js
+++ models/events.js
@@ -1,18 +1,58 @@
 import mongoose from "mongoose";
 
-//TODO: Add actual model
 var eventSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true } //adds createdAt and updatedAt automatically. See from DB
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		categories: {
+			type: [String],
+		},
+		dateTime: {
+			date: Array,
+			time: Array,
+			required: true,
+		},
+		location: {
+			latitude: Number,
+			longitude: Number,
+			name: String,
+			street: String,
+			locality: String,
+			subLocality: String,
+			administrativeArea: String,
+			subAdministrativeArea: String,
+			country: String,
+		},
+		eventFee: {
+			type: Number,
+			default: 0.0,
+		},
+		verificationState: {
+			isVerified: { type: Boolean, default: false },
+			verifiedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+			timestamps: true,
+		},
+		author: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			// required: true,
+		},
+		images: [
+			{
+				url: { type: String, required: true },
+				uploadedAt: { type: Date, default: Date.now() },
+				description: String,
+			},
+		],
+		registeredUsers: [mongoose.Schema.Types.ObjectId],
+	},
+	{ timestamps: true } //adds createdAt and updatedAt automatically. See from DB
 );
 
 const Event = mongoose.model("event", eventSchema);
--- models/validation_schema/auth_schema.js
+++ models/validation_schema/auth_schema.js
@@ -4,13 +4,14 @@ import Joi from "joi";
 
 //signup can contain additional properties
 const signupSchemaValid = Joi.object({
-  email: Joi.string().min(5).max(255).required().email(),
-  password: Joi.string().min(6).max(255).required(),
+	name: Joi.string().min(6).max(255).required(),
+	email: Joi.string().min(5).max(255).required().email(),
+	password: Joi.string().min(6).max(255).required(),
 });
 
 const loginSchemaValid = Joi.object({
-  email: Joi.string().min(5).max(255).required().email(),
-  password: Joi.string().min(6).max(255).required(),
+	email: Joi.string().min(5).max(255).required().email(),
+	password: Joi.string().min(6).max(255).required(),
 });
 
 export { signupSchemaValid, loginSchemaValid };
