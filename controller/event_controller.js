080c04bd0c613af34d6f551edce27cd78d3bf63b
sagar Uprety <sagarupreti100@gmail.com>
2021-07-26 22:57:33 +0545

    getParticipants

--- controller/event_controller.js
+++ controller/event_controller.js
@@ -3,141 +3,150 @@ import Organization from "../models/organization.js";
 
 //Get all Events
 const viewAllEvent = async (req, res) => {
-	try {
-		const event = await Event.find({}).sort({ createdAt: -1 }); //latest events will appear first.
-		if (event.length === 0) {
-			return res.json({ message: "No Events found", state: false });
-		} else {
-			return res.json({ event_list: event, state: true });
-		}
-	} catch (err) {
-		return res.json({ message: err, state: false });
-	}
+  try {
+    const event = await Event.find({}).sort({ createdAt: -1 }); //latest events will appear first.
+    if (event.length === 0) {
+      return res.json({ message: "No Events found", state: false });
+    } else {
+      return res.json({ event_list: event, state: true });
+    }
+  } catch (err) {
+    return res.json({ message: err, state: false });
+  }
 };
 
 //Create an Event
 const createEvent = async (req, res) => {
-	try {
-		if (req.user.organization) {
-			const organization = await Organization.findById(req.user.organization, {
-				_id: 1,
-				events: 1,
-			});
-			if (organization) {
-				const event = new Event(req.body);
-				event.author = req.user.organization;
-				organization.events.push(event._id);
-				await event.save();
-				await organization.save();
+  try {
+    if (req.user.organization) {
+      const organization = await Organization.findById(req.user.organization, {
+        _id: 1,
+        events: 1,
+      });
+      if (organization) {
+        const event = new Event(req.body);
+        event.author = req.user.organization;
+        organization.events.push(event._id);
+        await event.save();
+        await organization.save();
 
-				return res.json({ message: "Event Saved.", state: true });
-			}
-		}
-		return res.json({ message: "Organization not found.", state: false });
-	} catch (err) {
-		return res.json({ message: err, state: false });
-	}
+        return res.json({ message: "Event Saved.", state: true });
+      }
+    }
+    return res.json({ message: "Organization not found.", state: false });
+  } catch (err) {
+    return res.json({ message: err, state: false });
+  }
 };
 
 const register = async (req, res) => {
-	try {
-		const id = req.params.id;
-		const event = await Event.findById(id);
-		const currentUser = req.user;
+  try {
+    const id = req.params.id;
+    const event = await Event.findById(id);
+    const currentUser = req.user;
 
-		if (!event)
-			return res.json({ message: "Cannot Find Event.", state: false });
-		else {
-			var message = "";
-			if (!event.registeredUsers.includes(currentUser._id)) {
-				event.registeredUsers.push(currentUser._id);
-				currentUser.registeredEvents.push(event._id);
-				message = `Successfully registered to the event ${event.title.toUpperCase()}`;
-			} else {
-				event.registeredUsers.splice(
-					event.registeredUsers.indexOf(currentUser._id),
-					1
-				);
-				currentUser.registeredEvents.splice(
-					currentUser.registeredEvents.indexOf(event._id),
-					1
-				);
-				message = `Removed your registration to the event ${event.title.toUpperCase()}`;
-			}
-			await event.save();
-			await currentUser.save();
-			return res.json({ message: message, state: true });
-		}
-	} catch (err) {
-		return res.json({ message: err, state: false });
-	}
+    if (!event) return res.json({ message: "Cannot Find Event.", state: false });
+    else {
+      var message = "";
+      if (!event.registeredUsers.includes(currentUser._id)) {
+        event.registeredUsers.push(currentUser._id);
+        currentUser.registeredEvents.push(event._id);
+        message = `Successfully registered to the event ${event.title.toUpperCase()}`;
+      } else {
+        event.registeredUsers.splice(event.registeredUsers.indexOf(currentUser._id), 1);
+        currentUser.registeredEvents.splice(currentUser.registeredEvents.indexOf(event._id), 1);
+        message = `Removed your registration to the event ${event.title.toUpperCase()}`;
+      }
+      await event.save();
+      await currentUser.save();
+      return res.json({ message: message, state: true });
+    }
+  } catch (err) {
+    return res.json({ message: err, state: false });
+  }
 };
 
 const viewEventDetail = async (req, res) => {
-	try {
-		//TODO: try to put random string as eventID and see the error. Use JOI Validation so that we donot see that
-		const eventID = req.params.id;
-		const eventDetail = await Event.findById(eventID);
-		if (!eventDetail) {
-			console.log("Cannot Find Event");
-			return res.json({ message: "Cannot Find Event", state: false });
-		} else {
-			return res.json({ event: eventDetail, status: true });
-		}
-	} catch (err) {
-		return res.json({ message: err, state: false });
-	}
+  try {
+    //TODO: try to put random string as eventID and see the error. Use JOI Validation so that we donot see that
+    const eventID = req.params.id;
+    const eventDetail = await Event.findById(eventID);
+    if (!eventDetail) {
+      console.log("Cannot Find Event");
+      return res.json({ message: "Cannot Find Event", state: false });
+    } else {
+      return res.json({ event: eventDetail, status: true });
+    }
+  } catch (err) {
+    return res.json({ message: err, state: false });
+  }
 };
 
 //Search Events
 const searchEvents = async (req, res) => {
-	try {
-		const { text, category, date } = req.query;
+  try {
+    const { text, category, date } = req.query;
 
-		const events = await Event.find(
-			{
-				title: new RegExp(text, "i"),
-				categories: category ? { $in: category } : { $exists: true },
-				"dateTime.dates.0": date
-					? { $gte: date[0], $lte: date[1] }
-					: { $exists: true },
-			},
-			{ title: 1, categories: 1, dateTime: 1 }
-		);
+    const events = await Event.find(
+      {
+        title: new RegExp(text, "i"),
+        categories: category ? { $in: category } : { $exists: true },
+        "dateTime.dates.0": date ? { $gte: date[0], $lte: date[1] } : { $exists: true },
+      },
+      { title: 1, categories: 1, dateTime: 1 }
+    );
 
-		if (!events) return res.json({ message: "No events found.", state: false });
-		return res.json({ event_list: events, state: true });
-	} catch (err) {
-		console.log(err);
-		return res.json({ message: err, state: false });
-	}
+    if (!events) return res.json({ message: "No events found.", state: false });
+    return res.json({ event_list: events, state: true });
+  } catch (err) {
+    console.log(err);
+    return res.json({ message: err, state: false });
+  }
 };
 
 const deleteEvent = async (req, res) => {
-	try {
-		const deletedNews = await Event.findByIdAndDelete(req.params.id); //return deleted event
+  try {
+    const deletedNews = await Event.findByIdAndDelete(req.params.id); //return deleted event
 
-		if (!deletedNews) {
-			console.log("Event Not Found");
-			return res.json({ message: "Event Not Found", state: false }); //text
-		} else {
-			console.log("Event Deleted Successfully");
-			return res.json({
-				message: "Successfully Deleted the Event.",
-				state: true,
-			});
-		}
-	} catch (err) {
-		console.log(`Error occured while deleting due to ${err}`);
-		return res.json({ message: err, state: false });
-	}
+    if (!deletedNews) {
+      console.log("Event Not Found");
+      return res.json({ message: "Event Not Found", state: false }); //text
+    } else {
+      console.log("Event Deleted Successfully");
+      return res.json({
+        message: "Successfully Deleted the Event.",
+        state: true,
+      });
+    }
+  } catch (err) {
+    console.log(`Error occured while deleting due to ${err}`);
+    return res.json({ message: err, state: false });
+  }
+};
+
+const getParticpants = async (req, res) => {
+  try {
+    const eventID = req.params.id;
+    const event = await Event.findById(eventID);
+    const participants = await User.find({ _id: { $in: event.registeredUsers } });
+    if (!participants)
+      return res.json({
+        message: "No participants data found",
+        state: true,
+      });
+
+    return res.json({ user_list: participants, state: true });
+  } catch (error) {
+    return res.json({ message: error, state: false });
+  }
 };
 
 export default {
-	viewAllEvent,
-	createEvent,
-	register,
-	viewEventDetail,
-	searchEvents,
-	deleteEvent,
+  viewAllEvent,
+  createEvent,
+  register,
+  viewEventDetail,
+  searchEvents,
+  deleteEvent,
+  getParticpants,
 };
--- routes/event_routes.js
+++ routes/event_routes.js
@@ -18,12 +18,8 @@ router.post("/", checkUser, event_controller.createEvent);
 router.get("/search", authTokenCheck, event_controller.searchEvents);
 router.get("/:id", authTokenCheck, event_controller.viewEventDetail);
 router.post("/:id", checkUser, event_controller.register);
-router.post(
-	"/",
-	checkUser,
-	validation(createEventValid, "body"),
-	event_controller.createEvent
-);
+router.get("/participants/:id", authTokenCheck, event_controller.getParticpants);
+router.post("/", checkUser, validation(createEventValid, "body"), event_controller.createEvent);
 router.delete("/:id", authTokenCheck, event_controller.deleteEvent);
 
 export default router;
